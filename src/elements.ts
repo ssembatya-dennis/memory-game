1489bc5fd407a253e2b777ee7fa217e889ac8f93
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2023-05-06 11:15:39 +0300

    feat(elements.ts): add an element.ts file.
    - add an element.ts file that's responsible for handling selecting elements
    - the file is also responsible for exporting the selected elements to desired locations

---
+++ src/elements.ts
@@ -0,0 +1,42 @@
+type DivElement = HTMLDivElement | null;
+type ButtonElement = HTMLButtonElement | null;
+
+const moveContainerEl: DivElement = document.getElementById(
+  "move-count"
+) as HTMLDivElement;
+const timeContainerEl: DivElement = document.getElementById(
+  "time"
+) as HTMLDivElement;
+const startButtonEl: ButtonElement = document.getElementById(
+  "start"
+) as HTMLButtonElement;
+const reStartButtonEl: ButtonElement = document.getElementById(
+  "restart-btn"
+) as HTMLButtonElement;
+const newGameButtonEl: ButtonElement = document.getElementById(
+  "new-game"
+) as HTMLButtonElement;
+const startScreenContainerEl: DivElement = document.querySelector(
+  ".start-screen"
+) as HTMLDivElement;
+const gameContainerEl: DivElement = document.querySelector(
+  ".game-container"
+) as HTMLDivElement;
+const gameBoardContainerEl: DivElement = document.querySelector(
+  ".game-board"
+) as HTMLDivElement;
+const controlsContainerEl: DivElement = document.querySelector(
+  ".game-controls"
+) as HTMLDivElement;
+
+export {
+  moveContainerEl,
+  timeContainerEl,
+  startButtonEl,
+  reStartButtonEl,
+  newGameButtonEl,
+  startScreenContainerEl,
+  gameContainerEl,
+  gameBoardContainerEl,
+  controlsContainerEl,
+};
