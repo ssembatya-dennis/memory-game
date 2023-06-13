b0d5d11f2ed4c74db97fc50958c3ba87848ec9e7
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2023-06-13 16:17:27 +0300

    init(Logic): add toggle theme logic pharse one.

--- src/ui.ts
+++ src/ui.ts
@@ -15,7 +15,7 @@ export function buildUI() {
   updateGameScreen();
 }
 
-export function updateGameScreen() {
+function updateGameScreen() {
   if (state.gameState === "ongoing") {
     startScreenContainerEl?.classList.add("hide");
     gameContainerEl?.classList.remove("hide");
@@ -25,7 +25,19 @@ export function updateGameScreen() {
   }
 }
 
+function toggleTheme(theme: string) {
+  if (theme === "number") {
+    setState({ gameTheme: "numbers" });
+  }
+
+  if (theme === "icon") {
+    setState({ gameTheme: "icons" });
+  }
+}
+
 export function attachEventListeners() {
+  // add theme Btns event listeners
+
   // add start Btn event listeners
   startButtonEl?.addEventListener("click", () => {
     startGame();
